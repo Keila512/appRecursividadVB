e782cc6cf7b8c45a617f77c5ad473e72b470de41
Keila <Keila@ZURITA>
2023-08-31 16:10:28 -0600

    Add project files.

---
+++ appRecursividadVB/Module1.vb
@@ -0,0 +1,57 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim mat1 As Integer(,) = New Integer(2, 2) {}
+        Dim mat2 As Integer(,) = New Integer(2, 2) {}
+        Dim rnd As Random = New Random()
+
+        For i As Integer = 0 To 3 - 1
+
+            For j As Integer = 0 To 3 - 1
+                mat1(i, j) = rnd.[Next](1, 1000)
+                mat2(i, j) = rnd.[Next](1, 1000)
+            Next
+        Next
+
+        For i As Integer = 0 To 3 - 1
+
+            For j As Integer = 0 To 3 - 1
+                Console.Write(mat1(i, j) & vbTab)
+            Next
+
+            Console.WriteLine()
+        Next
+
+        Console.WriteLine()
+
+        For i As Integer = 0 To 3 - 1
+
+            For j As Integer = 0 To 3 - 1
+                Console.Write(mat2(i, j) & vbTab)
+            Next
+            Console.WriteLine()
+        Next
+        Console.WriteLine("THE SUM OF THE DATA OF THE MATRIXES IS: " & matricesRecursiveSum(mat1, mat2, 2, 2))
+        Console.ReadKey()
+
+    End Sub
+    Private Function matricesRecursiveSum(ByVal mat1 As Integer(,), ByVal mat2 As Integer(,), ByVal nf As Integer, ByVal nc As Integer) As Integer
+        If (nf = 0) AndAlso (nc = 0) Then
+            Return mat1(nf, nc) + mat2(nf, nc)
+        Else
+
+            If nf > -1 Then
+                nc -= 1
+
+                If nc >= -1 Then
+                    Return mat1(nf, nc + 1) + mat2(nf, nc + 1) + matricesRecursiveSum(mat1, mat2, nf, nc)
+                Else
+                    Return matricesRecursiveSum(mat1, mat2, nf - 1, 2)
+                End If
+            Else
+                Return 0
+            End If
+        End If
+    End Function
+
+End Module
---
+++ appRecursividadVB/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("appRecursividadVB")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("appRecursividadVB")>
+<Assembly: AssemblyCopyright("Copyright ©  2023")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("041f6a82-2a52-4de4-a6a1-fce9fff5d655")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
